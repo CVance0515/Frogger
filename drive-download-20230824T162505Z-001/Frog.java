ef7ff152338700eb84de81fc940cb2e23ffba3f7
ScottDaKitsune <142824158+ScottDaKitsune@users.noreply.github.com>
2023-08-25 16:23:37 +0000

    Added Frog constructor

--- drive-download-20230824T162505Z-001/Frog.java
+++ drive-download-20230824T162505Z-001/Frog.java
@@ -2,4 +2,9 @@ import objectdraw.*;
 import java.awt*;
 public class Frog{
   private VisibleImage frogImg;
+  private static final double FROG_HEIGHT = 48;
+
+  public Frog(Image i, DrawingCanvas c){
+    frogImage = new VisibleImage(i, new Location(10,10), c);
+  }
 }
