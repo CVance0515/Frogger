a49008ca8d4b5d7f96a57b379248b2f33e0f0fed
MaximilianoVillafranco <142824359+MaximilianoVillafranco@users.noreply.github.com>
2023-08-30 16:45:58 +0000

    made road fit screen

--- drive-download-20230824T162505Z-001/Main.java
+++ drive-download-20230824T162505Z-001/Main.java
@@ -13,9 +13,9 @@ public class Main extends WindowController implements KeyListener{
    private static final double LINE_WIDTH = LANE_WIDTH / 10;
    
    // Constants defining the locations of the background components
-   private static final double HIGHWAY_LEFT = 50;
+   private static final double HIGHWAY_LEFT = 0;
    private static final double HIGHWAY_RIGHT = HIGHWAY_LEFT + HIGHWAY_LENGTH;
-   private static final double HIGHWAY_TOP = 100;
+   private static final double HIGHWAY_TOP = 0;
    private static final double HIGHWAY_BOTTOM = HIGHWAY_TOP + HIGHWAY_WIDTH;
    
    // Constants describing the lines on the highway
@@ -27,7 +27,7 @@ public class Main extends WindowController implements KeyListener{
    // instructions to create the frog and the Lane ActiveObjects.
    
    public static void main(String[] args) { 
-   new Main().startController(400,400); 
+   new Main().startController(700,400); 
 	} 
    
    public void begin()
