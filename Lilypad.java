4a6874837d75b2c542331f492beb44629c602f35
ScottDaKitsune <142824158+ScottDaKitsune@users.noreply.github.com>
2023-09-05 16:40:16 +0000

    Attempt 1 to update lilypad with frog

--- Lilypad.java
+++ Lilypad.java
@@ -7,9 +7,17 @@ public class Lilypad{
   //state variables
   private VisibleImage lilyImg;
   private int lilyX;
+  private Frog Bob;
   //Lilypad constructor
-  public Lilypad(Image i, DrawingCanvas c, int x){
+  public Lilypad(Image i, DrawingCanvas c, int x, Frog bob){
     lilyImg = new VisibleImage(i, new Location(x, 10), 100, 60, c);
     int lilyX = x;
+    Bob = bob;
+  }
+
+  public void updateLily(Image i, DrawingCanvas c, int x){
+    if(lilyImg.collidesWith(Bob.getVisibleImage())){
+      lilyImg = new VisibleImage(i, new Location(x, 10), 100, 60, c);
+    }
   }
 }
