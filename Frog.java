c54c4f78a711f59bf6347bb81ad101ea7392f5fd
MaximilianoVillafranco <142824359+MaximilianoVillafranco@users.noreply.github.com>
2023-09-05 16:37:14 +0000

    added getVisibleImage method to Frog.java

--- Frog.java
+++ Frog.java
@@ -7,33 +7,46 @@ public class Frog {
   
   private VisibleImage frogImg;
   private static final double FROG_HEIGHT = 48;
-
+  private int lives = 3;
+  private boolean alive = true;
   public Frog(Image i, DrawingCanvas c){
     frogImg = new VisibleImage(i, new Location(10,10), c);
   }
   public boolean overlaps(VisibleImage vehicleImg){
     return frogImg.overlaps(vehicleImg);
   }
+  public VisibleImage getVisibleImage(){
+    return frogImg;
+  }
   public void hops(String direction){
-    if(direction.equals("left")){
-      if(frogImg.getX() != 10){
-        frogImg.move(-100,0);
+    if(alive){
+      if(direction.equals("left")){
+        if(frogImg.getX() != 10){
+          frogImg.move(-100,0);
+        }
+      }
+      if(direction.equals("right")){
+        if(frogImg.getX() != 610){
+          frogImg.move(100,0);
+        }
+      }
+      if(direction.equals("up")){
+        if(frogImg.getY() != 10){
+          frogImg.move(0,-70);
+        }
+      }
+      if(direction.equals("down")){
+        if(frogImg.getY() != 360){
+          frogImg.move(0,70);
+        }
       }
     }
-    if(direction.equals("right")){
-      if(frogImg.getX() != 610){
-        frogImg.move(100,0);
-      }
+    if( !alive && lives > 0){
+      lives--;
+      alive = true;
     }
-    if(direction.equals("up")){
-      if(frogImg.getY() != 10){
-        frogImg.move(0,-70);
-      }
-    }
-    if(direction.equals("down")){
-      if(frogImg.getY() != 360){
-        frogImg.move(0,70);
-      }
+    if( !alive && lives == 0){
+      
     }
   }
 }
