023bc952383b07b1351a715d40a03619e5fa68c5
CVance0515 <133129512+CVance0515@users.noreply.github.com>
2023-09-18 11:44:17 -0500

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -3,17 +3,51 @@ import java.awt.event.*;
 import java.awt.Color;
 import java.awt.Image;
 
-public class Vehicle {
+public class Vehicle extends ActiveObject{
   
   private VisibleImage vehicleImg;
   private static final double VEHICLE_HEIGHT = 48;
+  private double firstTime;
+  private double finalTime;
+  private double velo;
+  
 
-  public Vehicle(Image i, DrawingCanvas c){
+  public Vehicle(Image i, DrawingCanvas c, double xLoc, double yLoc, double velocity, int maxDist, Frog frogVar) {
     vehicleImg = new VisibleImage(i, new Location(550,70), c);
+
+    velo = velocity;
+    
+    
+    this.start();
   }
-  /*
-  public boolean overlaps(VisibleImage vehicleImg){
-    return vehicleImg.overlaps(vehicleImg);
+
+  public boolean overlaps(VisibleImage frogImg){
+    return vehicleImg.overlaps(frogImg);
   }
-  */
+  
+  public static void wait(int ms)
+  {
+      try
+      {
+          Thread.sleep(ms);
+      }
+      catch(InterruptedException ex)
+      {
+          Thread.currentThread().interrupt();
+      }
+  }
+  
+  public void run() {
+    while(vehicleImg.getX() > -100 && vehicleImg.getX() < 800) {
+      firstTime = System.currentTimeMillis();
+      wait(30);
+      finalTime = System.currentTimeMillis() - firstTime;
+     
+      double distance = finalTime * velo / 3;
+      //System.out.println(distance);
+      vehicleImg.move(distance,0);
+      // vehicleImg.move(-5,0);
+      
+    }
+  }
 }
